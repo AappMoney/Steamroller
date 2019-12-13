3b0a5738880bc4ae17ee3205976a3688223b298b
AappMoney <46736553+AappMoney@users.noreply.github.com>
2019-12-13 22:26:08 +0500

    Create hasNext.js

---
+++ hasNext.js
@@ -0,0 +1,10 @@
+const CULLARR = [2, [4, [6]]];
+
+function hasNext(array){
+    return array.reduce((total, currentValue) => {
+        return Array.isArray(currentValue) ? [...total, ...hasNext(currentValue)] : [...total, currentValue];
+    },[]);
+}
+
+
+console.log(hasNext(CULLARR));
