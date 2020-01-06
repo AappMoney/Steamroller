67b0f163b634685f20ae72665a2d936e3b978737
AappMoney <46736553+AappMoney@users.noreply.github.com>
2020-01-06 23:12:33 +0500

    Update hasNext.js

--- hasNext.js
+++ hasNext.js
@@ -8,3 +8,16 @@ function hasNext(array){
 
 
 console.log(hasNext(CULLARR)); // [ 2, 4, 6 ]
+
+
+// /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+
+
+function steamrollArray(arr) {
+  while (arr.some(element => Array.isArray(element))) {
+    arr = arr.flat();
+  }
+  return arr;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]); // [ 1, 2, 3, 4 ]
